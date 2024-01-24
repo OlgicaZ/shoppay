2dbc4ef9d1ae94f247dc7e95f1ac5441d45b67e0
Олгица Златковска <olgica@Olgicas-MacBook-Pro.local>
2024-01-24 14:33:04 -0500

    add and style nav elements

---
+++ components/header/Top.js
@@ -0,0 +1,46 @@
+import styles from './styles.module.scss';
+import { MdOutlineSecurity } from "react-icons/md";
+import { BsSuitHeart } from "react-icons/bs";
+import { RiAccountPinCircleLine } from "react-icons/ri";
+import { RiArrowDropDownFill } from "react-icons/ri";
+
+export default function Top() {
+    return (
+        <div className={styles.top}>
+            <div className={styles.top__container}>
+                <div></div>
+                <ul className={styles.top__list}>
+                    <li className={styles.top__list_item}>
+                        <img
+                            className={styles.top__country_img}
+                            src='https://upload.wikimedia.org/wikipedia/commons/7/79/Flag_of_North_Macedonia.svg'
+                            alt='yellow sun on a red background'
+                        />
+                        <span>Macedonia / USD</span>
+                    </li>
+                    <li className={styles.top__list_item}>
+                        <MdOutlineSecurity />
+                        <span>Buyer Protection</span>
+                    </li>
+                    <li className={styles.top__list_item}>
+                        <span>Customer Service</span>
+                    </li>
+                    <li className={styles.top__list_item}>
+                        <span>Help</span>
+                    </li>
+                    <li className={styles.top__list_item}>
+                        <BsSuitHeart />
+                        <span>Wishlist</span>
+                    </li>
+                    <li className={styles.top__list_item}>
+                        <div className={styles.flex}>
+                            <RiAccountPinCircleLine />
+                            <span>Account</span>
+                            <RiArrowDropDownFill />
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
--- components/header/index.js
+++ components/header/index.js
@@ -1,3 +1,4 @@
+import Top from './Top';
 import Ad from './ad';
 import styles from './styles.module.scss';
 
@@ -5,6 +6,7 @@ export default function Header() {
     return (
         <header className={styles.header}>
             <Ad />
+            <Top />
         </header>
     );
 }
