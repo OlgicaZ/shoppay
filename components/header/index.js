6304076b0a7fa75d7878e43313f45a284e956bdc
Олгица Златковска <olgica@Olgicas-MacBook-Pro.local>
2024-01-24 13:34:37 -0500

    add ad component to header

--- components/header/index.js
+++ components/header/index.js
@@ -1,5 +1,10 @@
+import Ad from './ad';
+import styles from './styles.module.scss';
+
 export default function Header() {
     return (
-        <></>
+        <header className={styles.header}>
+            <Ad />
+        </header>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.scss";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      
-      <h1 className={styles.red}>Welcome</h1>
+      <Header />
     </>
   );
 }
