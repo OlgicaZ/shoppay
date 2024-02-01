ed6f71cb09d7318a3f670b9b45c9a988a99b623a
Олгица Златковска <olgica@Olgicas-MacBook-Pro.local>
2024-02-01 12:39:38 -0500

    nextauth setup

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,20 @@ import styles from "@/styles/Home.module.scss";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import axios from 'axios';
+import { useSession, signIn, signOut } from "next-auth/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ country }) {
+  
+  const { data: session } = useSession()
+  
   return (
     <>
       <Header country={country} />
+      {
+        session ? "you are logged in" : "you are not logged in"
+      }
       <Footer country={country} />
     </>
   );
