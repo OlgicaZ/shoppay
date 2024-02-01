e2f7b31cfdc2850740a0635a7531c4c117501eb2
Олгица Златковска <olgica@Olgicas-MacBook-Pro.local>
2024-02-01 17:22:10 -0500

    add sigin header

--- pages/signin.js
+++ pages/signin.js
@@ -1,5 +1,24 @@
+import styles from './../styles/signin.module.scss';
+import { BiLeftArrowAlt } from "react-icons/bi";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import Link from 'next/link';
+
 export default function signin() {
     return (
-        <div>SIGNIN</div>
+        <>
+            <Header country={{}} />
+            <div className={styles.login}>
+                <div className={styles.login__container}>
+                    <div className={styles.login__header}>
+                        <div className={styles.back__svg}>
+                            <BiLeftArrowAlt />
+                        </div>
+                        <span>We'd be happy to join us! <Link href='/'>Go Store</Link></span>
+                    </div>
+                </div>
+            </div>
+            <Footer country={{}} />
+        </>
     )
 }
