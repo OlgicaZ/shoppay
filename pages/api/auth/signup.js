a4709200a7d4ad5ede257799808c3cbb107319f4
Олгица Златковска <olgica@Olgicas-MacBook-Pro.local>
2024-02-14 12:29:04 -0500

    create user model

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,6 +1,7 @@
 import { createRouter } from "next-connect";
 import db from './../../../utils/db';
 import { validateEmail } from "@/utils/validation";
+import User from "@/models/User";
 
 const router = createRouter()
 
@@ -17,6 +18,15 @@ router.post(async (req, res) => {
             return res.status(400).json({message: 'Invalid email'});
         }
 
+        const user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({message: 'This email already exist'});
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({message: 'Password must be at lest 6 characters'});
+        }
 
     } catch (error) {
         res.status(500).json({message: error.message});
